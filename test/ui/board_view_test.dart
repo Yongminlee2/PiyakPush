9b79e863e9eee6eb5b3e01b97c085726b7d2aedf
Yongminlee2 <dydals5678@gmail.com>
2026-08-02 23:30:33 +0900

    연속 이동: 홀드 중 등속 + 뒤뚱·착지 스쿼시 + 알 반동

--- test/ui/board_view_test.dart
+++ test/ui/board_view_test.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:piyak_push/engine/board.dart';
 import 'package:piyak_push/ui/widgets/board_view.dart';
 import 'package:piyak_push/ui/widgets/tile_painter.dart';
 
 void main() {
-  testWidgets('BoardView 스모크: 타일·알·병아리 렌더링', (tester) async {
+  testWidgets('BoardView ?ㅻえ?? ??셋룹븣쨌蹂묒븘由??뚮뜑留?, (tester) async {
     final board = Board.fromAscii(['#####', '#@\$o#', '#i.c#', '#####']);
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
@@ -14,8 +14,8 @@ void main() {
     ));
     await tester.pump(const Duration(milliseconds: 200));
     expect(find.byType(BoardView), findsOneWidget);
-    expect(find.byType(EggWidget), findsOneWidget);
-    expect(find.byType(Image), findsOneWidget); // 병아리
+    expect(find.byType(EggSprite), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget); // 蹂묒븘由?
     expect(tester.takeException(), isNull);
   });
 }
